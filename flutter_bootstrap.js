4f5e0857d4b782eba2d032dd61eccf0da1502f62
조동건 <77666879+CDGDG@users.noreply.github.com>
2024-12-22 00:24:10 +0900

    feat: Update projects

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "726459469"
+    serviceWorkerVersion: "1711517385"
   }
 });
