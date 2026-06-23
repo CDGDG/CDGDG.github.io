0102a8e3abeaf88e50318a2732bb34a96742ce70
cdgdg <whehdrjs953964@gmail.com>
2026-06-23 22:49:45 +0900

    Deploy Huggies Engineering thought

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "394462232" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2163703788" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
