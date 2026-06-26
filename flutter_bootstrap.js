ddf54f3d32f6c3420adea89715cc730239c51ee4
조동건 <77666879+CDGDG@users.noreply.github.com>
2026-06-26 10:43:21 +0900

    Deploy Huggies + Dynamic Sub-Agent brain nodes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4294601239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3365560797" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
