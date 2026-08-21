4d1aeb96e24e3998247956c8e238493c6c3e2633
cdgdg <whehdrjs953964@gmail.com>
2026-08-21 10:44:05 +0900

    Deploy restored white portfolio with Bbungiyo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3604988692" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "938634885" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
